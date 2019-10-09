f2e075e85c2bc0a0d7ac948ddaac4919c86dd255
DevExpressExampleBot <fakeEmail>
2019-10-09 15:36:34 +0300

    Source auto update [skip ci]

--- VB/DevAVInvoicing/Program.vb
+++ VB/DevAVInvoicing/Program.vb
@@ -5,21 +5,17 @@ Imports System.Threading.Tasks
 Imports System.Windows.Forms
 
 Namespace DevAVInvoicing
-    Friend NotInheritable Class Program
-
-        Private Sub New()
-        End Sub
-
-        ''' <summary>
-        ''' The main entry point for the application.
-        ''' </summary>
-        <STAThread> _
-        Shared Sub Main()
-            DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Office 2016 Colorful")
-            DevExpress.Skins.SkinManager.EnableFormSkins()
-            Application.EnableVisualStyles()
-            Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New Form1())
-        End Sub
-    End Class
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
+			DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Office 2016 Colorful")
+			DevExpress.Skins.SkinManager.EnableFormSkins()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+	End Module
 End Namespace
